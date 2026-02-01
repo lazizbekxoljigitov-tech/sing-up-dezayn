6513a35ed968965b4b34d387e2bafaab4a5ddb4f
dadaxanov ubaydulloh <lazizbekxoljigitov@gmail.com>
2026-02-01 14:25:07 +0500

    tayyor

--- js/app.js
+++ js/app.js
@@ -1,2 +1,4 @@
 const cardsTrack = document.querySelector(".cards__track");
 cardsTrack.innerHTML += cardsTrack.innerHTML;
+const track = document.querySelector(".hero__track");
+track.innerHTML += track.innerHTML;
